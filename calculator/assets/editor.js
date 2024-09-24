6a453d6719f3b662d373795826f58d5f6e3cdb9c
Niño James A. Tan <njtan142@gmail.com>
2024-09-24 04:19:29 +0000

    debug: calculator

--- calculator/assets/editor.js
+++ calculator/assets/editor.js
@@ -2,6 +2,7 @@ import AlgebraLatex from "./parser/index.js";
 
 var mathFieldSpan = document.getElementById("math-field");
 var answerElement = document.getElementById("answer");
+var consoleElement = document.getElementById("console");
 
 var expression = new AlgebraLatex();
 
@@ -12,37 +13,17 @@ var mathField = MQ.MathField(mathFieldSpan, {
     edit: function () {
       try {
         let latex = mathField.latex();
-        // if (latex.includes("!")) {
-        //   let f_index = latex.indexOf("!") - 1; // Start from the character before '!'
-        //   let f_org_index = f_index;
-        //   let f_number = "";
-
-        //   // Read backwards to collect the number
-        //   while (f_index >= 0 && !isNaN(latex[f_index])) {
-        //     f_number = latex[f_index] + f_number; // Prepend the character to f_number
-        //     f_index--;
-        //   }
-
-        //   console.log(f_number, f_index + 1, f_org_index); // Outputs the extracted number for the factorial
-        //   const factorial = Algebrite.run(f_number + "!");
-        //   console.log(factorial.toString());
-
-        //   const beforeFactorial = latex.slice(0, f_index + 1); // Part before the number
-        //   const afterFactorial = latex.slice(f_org_index + 2); // Part after the '!'
-        //   let newLatex = beforeFactorial + factorial + afterFactorial;
-        //   console.log(newLatex);
-        //   latex = newLatex;
-        // }
         latex = evalFactorial(latex, 0, countFactorials(latex));
         expression = new AlgebraLatex().parseLatex(latex);
         let result = expression.toAlgebrite(Algebrite);
         const answer = Algebrite.run(result.toString());
         const answerLatex = new AlgebraLatex().parseMath(answer.toString());
-        console.log(answer.toString());
+        console.log(result.toString(), answer.toString());
         answerElement.innerHTML = answerLatex.toLatex();
         MQ.StaticMath(answerElement);
       } catch (error) {
         console.log(error);
+        consoleElement.innerHTML = error.toString();
       }
     },
   },
@@ -63,14 +44,14 @@ function evalFactorial(latex, pass = 0, max = 10) {
       f_index--;
     }
 
-    console.log(f_number, f_index + 1, f_org_index); // Outputs the extracted number for the factorial
+    //console.log(f_number, f_index + 1, f_org_index); // Outputs the extracted number for the factorial
     const factorial = Algebrite.run(f_number + "!");
-    console.log(factorial.toString());
+    //console.log(factorial.toString());
 
     const beforeFactorial = latex.slice(0, f_index + 1); // Part before the number
     const afterFactorial = latex.slice(f_org_index + 2); // Part after the '!'
     let newLatex = beforeFactorial + factorial + afterFactorial;
-    console.log(newLatex);
+    //console.log(newLatex);
     latex = newLatex;
     return evalFactorial(latex, pass + 1, max);
   }
